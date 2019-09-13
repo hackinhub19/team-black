2eebd53f6c06816c6f7bf2c9581b9cd8e6b5daab
nitish <nitishv509@gmail.com>
2019-09-13 20:29:18 +0530

    first commit

--- sai_ram_hack/sai_ram_hack/config/config.js
+++
@@ -1,6 +0,0 @@
-module.exports = {
-    PORT: process.env.PORT || 8080,
-    MONGO_URL: process.env.MONGO_URL || "mongodb+srv://root:nitish123@cluster0-kgyzb.mongodb.net/test?retryWrites=true&w=majority",
-    DEVELOPER: process.env.DEVELOPER || "NITISH V",
-    BASE_URL: process.env.BASE_URL || "http://localhost"
-}
